69bb235a3a386d57b1aea63d95754376728509f6
rnaddy <rnaddy@corp.acesse.com>
2014-03-21 17:57:34 -0500

    Added Caching, and updated Mysql

--- Websites/zingphp.com/Pages/Home.php
+++ Websites/zingphp.com/Pages/Home.php
@@ -3,10 +3,8 @@
 class Home extends Zing{
 
     public function main(){
-        $this->fcache->init("test");
-
-        $this->fcache->cache(10, function(){
-            //echo "<p><b>Running Cache</b></p>";
+        $cache = $this->fcache->init("test")->cache(10, function(){
+            echo "<p><b>Running Cache</b></p>";
             return array(
                 "hello",
                 "how",
@@ -14,7 +12,7 @@ class Home extends Zing{
                 "you"
             );
         });
-        var_dump($this->fcache->get());
+        var_dump($cache);
     }
 
 }
