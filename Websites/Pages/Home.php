6c4a8ac7c433bc390a7337809adf5936b0ab3f83
Ryan <untuned20@gmail.com>
2014-09-13 21:33:27 -0500

    Updated Typo

--- Websites/Pages/Home.php
+++ Websites/Pages/Home.php
@@ -4,8 +4,8 @@ class Home extends Zing{
 
     public function main(){
         
-        echo "<h1>Zing</h1>";
-        echo "<p>Welcome to the Zing framework! Look like all is well!</p>";
+        echo "<h1>Success!</h1>";
+        echo "<p>Welcome to the Zing framework! Looks like all is well!</p>";
         
     }
 
