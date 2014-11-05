7305230d4d17f6f2da9d8d690121182713daf5b7
Ryan Naddy <rnaddy@corp.acesse.com>
2014-11-05 15:40:27 -0600

    Updated Mail class

--- Zing/src/Modules/Mail.php
+++ Zing/src/Modules/Mail.php
@@ -2,10 +2,13 @@
 
 namespace Modules;
 
+use Exception;
+
 class Mail extends Module{
 
     protected $attachments = array();
     protected $recipients  = array();
+    protected $email, $name;
 
     /**
      * Adds files to be sent with the message
@@ -32,9 +35,9 @@ class Mail extends Module{
      * @return \Mail
      */
     public function addRecipients(array $recipients, &$valid = 0, &$invalid = 0){
-        foreach($recipients as $name => $email){
+        foreach($recipients as $email => $name){
             if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-                $this->recipients[$name] = $email;
+                $this->recipients[$email] = $name;
                 $valid++;
             }else{
                 $invalid++;
@@ -44,31 +47,46 @@ class Mail extends Module{
     }
 
     /**
+     * Set the sender information
+     * @param type $email
+     * @param type $name
+     * @return Mail
+     * @throws Exception
+     */
+    public function setSender($email, $name = ""){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $this->email = $email;
+        }else{
+            throw new Exception("'$email' is not a valid email address.");
+        }
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
      * Sends a message to the list of recipients.<br>
      * Note: Once the email is sent, everything gets reset.
-     * @param array $from       Who the email is from
-     *                          array("email" => "ex@site.com", "name" => "Billy Bob")
      * @param string $subject   The subject of the message
      * @param string $message   The body of the message
      * @throws Exception
      * @return \Mail
      */
-    public function send(array $from, $subject = "", $message = ""){
+    public function send($subject = "", $message = ""){
         if(count($this->recipients) == 0){
             throw new Exception("No recipents set. Emails must be valid.");
         }
-        if(!filter_var($from["email"], FILTER_VALIDATE_EMAIL)){
-            throw new Exception("'{$from["email"]}' is not a valid email address.");
+        if(empty($this->email)){
+            throw new Exception("No sender set.");
         }
         if(!empty($this->attachments)){
             $random_hash   = md5(date('r', time()));
             $mime_boundary = "==Multipart_Boundary_x{$random_hash}x";
         }
-        foreach($this->recipients as $name => $email){
+        foreach($this->recipients as $email => $name){
             $headers = "MIME-Version: 1.0\r\n";
             $headers .= "To: $name <$email>\r\n";
-            $name    = isset($from["name"]) ? $from["name"] : "";
-            $headers .= "From: $name <{$from["email"]}>\r\n";
+            $name    = isset($this->name) ? $this->name : "";
+            $headers .= "From: $name <$this->email>\r\n";
             if(!empty($this->attachments)){
                 $headers .= "Content-Type: multipart/mixed; boundary=\"$mime_boundary\";\r\n";
                 $msg_final = "--$mime_boundary\r\n" .
